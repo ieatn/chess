abcb138ff3015e93ba5ff4746a5f5dd800c2f555
ieatn <ieatnubcake@gmail.com>
2023-04-07 20:36:44 -0700

    added piece image

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -5,13 +5,27 @@ const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
+interface Piece {
+    image: string
+    x: number
+    y: number
+}
+const pieces: Piece[] = []
+pieces.push({ image:'images/pawn_b.png', x: 0, y: 1 })
+
 export default function Chessboard() {
 
     let board = []
     for (let i = 0; i < horizontalAxis.length; i++) {
         for (let j  = 0; j < verticalAxis.length; j++) {
+            
             const number = j + i + 2
-            board.push(<Tile number={number}/>)
+            // console.log(i, j)
+            // if (i == 0 && j == 1) {
+
+            //     board.push(<Tile number={number} image={pieces[0].image} />)
+            // }
+            board.push(<Tile number={number} image={pieces[0].image} />)
         }
     }
     
